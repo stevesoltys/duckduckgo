2d6ee7e2ae6b690278cf352ca10e27fdc5bb1ebe
Manabu Niseki <manabu.niseki@gmail.com>
2018-12-16 20:43:59 +0900

    fix HTML parsing issue
    The current implementation of HTML parsing causes some issues (#5 and #6). So I fix them.

--- test/test_search.rb
+++ test/test_search.rb
@@ -15,6 +15,13 @@ class TestSearch < Minitest::Unit::TestCase
   def test_search
     results = DuckDuckGo::search(:query => TEST_SEARCH_PHRASE)
     assert(!results.empty?, "Searching for '#{TEST_SEARCH_PHRASE}' returned zero results.")
+
+    # it should be biggenr than 1
+    assert_operator(results.length, :>, 1)
+
+    result = results.first
+    # it shoul be equal to "https://en.wikipedia.org/wiki/Apple"
+    assert_equal(result.uri, "https://en.wikipedia.org/wiki/Apple")
   end
 
   ##
